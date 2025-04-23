6ea9747635be83feeaea698f8491ad55af795b11
titauhidd <tauhidultonmoy1234@gmail.com>
2025-04-23 13:52:18 +0600

    loaded data from api with async await

---
+++ js/first.js
@@ -0,0 +1,25 @@
+// function loaddata() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.json())
+//     .then((data) => loaddata2(data));
+// }
+
+// function loaddata2(data) {
+//   const userd = document.getElementById("userContainer");
+//   for (const user of data) {
+//     // console.log(user.email);
+//     const userDiv = document.createElement("div");
+//     userDiv.classList.add("post");
+//     userDiv.innerHTML = `
+//     <h2>It's Users Mail :- ${user.email}</h2>
+//     <h2>It's Users Name :- ${user.name}</h2>
+//     `;
+//     userd.appendChild(userDiv);
+//   }
+// }
+
+async function loaddata() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+}
